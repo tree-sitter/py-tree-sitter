d73671c09cc00a7c68ec6740188a346d7cc06cfa
Max Brunsfeld <maxbrunsfeld@gmail.com>
2019-04-29 10:43:53 -0700

    0.0.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, Extension
 
 setup(
     name = "tree_sitter",
-    version = "0.0.4",
+    version = "0.0.5",
     maintainer = "Max Brunsfeld",
     maintainer_email = "maxbrunsfeld@gmail.com",
     author = "Max Brunsfeld",
