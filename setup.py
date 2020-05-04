dd07df5434a9795e6079707cbbb4a048c4a3885a
Max Brunsfeld <maxbrunsfeld@gmail.com>
2020-05-04 10:15:53 -0700

    0.1.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(path.dirname(__file__), "README.md")) as f:
 
 setup(
     name="tree_sitter",
-    version="0.1.0",
+    version="0.1.1",
     maintainer="Max Brunsfeld",
     maintainer_email="maxbrunsfeld@gmail.com",
     author="Max Brunsfeld",
