c1d112686dc1f187222467b91cab2f7ad4b5a943
Josh Handley <josh@teleyah.com>
2024-02-26 19:17:22 +0000

    feat: make match captures a dictionary (#165)
    Return the captures of a match as a dict where the key is the capture
    name and the value is a Node or a list of Nodes. For regular captures,
    the value is a Node. For captures that can contain multiple nodes
    because they use a * or + quantifier, the value is a list of Nodes.

--- tree_sitter/_binding.pyi
+++ tree_sitter/_binding.pyi
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Any, Callable, Iterable, List, Optional, Tuple
+from typing import Any, Callable, Dict, Iterable, List, Optional, Tuple, Union
 
 import tree_sitter
 
@@ -349,7 +349,7 @@ class Query:
         end_point: Optional[Tuple[int, int]] = None,
         start_byte: Optional[int] = None,
         end_byte: Optional[int] = None,
-    ) -> List[Tuple[int, List[Tuple[Node, str]]]]:
+    ) -> List[Tuple[int, Dict[str, Union[Node, List[Node]]]]]:
         """Get a list of all of the matches within the given node."""
 
     def captures(
