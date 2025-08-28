d36bde57f944a241b4ad164e47143503cf5a68ea
ObserverOfTime <chronobserver@disroot.org>
2025-08-28 18:54:56 +0300

    build: properly detect MSVC compiler

--- setup.py
+++ setup.py
@@ -1,26 +1,34 @@
-from os import name as os_name
 from platform import machine
 
 from setuptools import Extension, setup  # type: ignore
+from setuptools.command.build_ext import build_ext
 
-if os_name != "nt":
-    cflags = [
-        "-std=c11",
-        "-fvisibility=hidden",
-        "-Wno-cast-function-type",
-        "-Werror=implicit-function-declaration",
-    ]
-    # FIXME: GCC optimizer bug workaround for #330 & #386
-    if machine().startswith("aarch64"):
-        cflags.append("--param=early-inlining-insns=9")
-else:
-    cflags = ["/std:c11", "/wd4244"]
+
+class BuildExt(build_ext):
+    def build_extension(self, ext: Extension):
+        if self.compiler.compiler_type != "msvc":
+            ext.extra_compile_args = [
+                "-std=c11",
+                "-fvisibility=hidden",
+                "-Wno-cast-function-type",
+                "-Werror=implicit-function-declaration",
+            ]
+            # FIXME: GCC optimizer bug workaround for #330 & #386
+            if machine().startswith("aarch64"):
+                ext.extra_compile_args.append("--param=early-inlining-insns=9")
+        else:
+            ext.extra_compile_args = ["/std:c11", "/wd4244"]
+        super().build_extension(ext)
+
 
 setup(
     packages=["tree_sitter"],
     include_package_data=False,
     package_data={
         "tree_sitter": ["py.typed", "*.pyi"],
+    },
+    cmdclass={
+        "build_ext": BuildExt,
     },
     ext_modules=[
         Extension(
@@ -50,7 +58,6 @@ setup(
                 ("PY_SSIZE_T_CLEAN", None),
                 ("TREE_SITTER_HIDE_SYMBOLS", None),
             ],
-            extra_compile_args=cflags,
         )
     ],
 )
